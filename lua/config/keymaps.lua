8e3209de83db767cdf126d7f9d47834dc05313d0
HowXu <howxu366@outlook.com>
2026-05-21 19:10:04 +0800

    feat: minimap quick sets

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,8 @@
 --
 -- No macro
 vim.keymap.set("n", "q", "<Nop>")
+
+-- MiniMap quick
+vim.keymap.set('n', '<leader>m', '<Nop>', { desc = "MiniMap Quick Set" })
+vim.keymap.set('n', '<leader>md', '<cmd>Neominimap Disable<CR>', { desc = "Disable Neominimap" })
+vim.keymap.set('n', '<leader>me', '<cmd>Neominimap Enable<CR>', { desc = "Enable Neominimap" })
